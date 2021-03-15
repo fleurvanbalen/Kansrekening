b54ad2fd6916e2dbfec5b8bd8441e81b51c2f489
Fleur van Balen <fleur.vanbalen@student.ru.nl>
2021-03-15 21:21:27 +0100

    W7

--- W7-FleurvanBalen.py
+++ W7-FleurvanBalen.py
@@ -1,9 +1,10 @@
 import random as rd
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
 
 # Opdracht 1: Een functie voor m aantal getallen Z_n
-# voor een stochast X_k geldt: verwachting mu = 0,5 en variantie sigma^2 = 1/12, dus sigma = sqrt(1/12)
+# voor een stochast X_k ~ Un(0,1) geldt: verwachting mu = 0,5 en variantie sigma^2 = 1/12, dus sigma = sqrt(1/12)
 mu = 0.5
 sigma_2 = 1.0/12.0
 
@@ -24,9 +25,30 @@ Z_10 = simuleer(10, 100000)
 randen = np.linspace(-3.5, 3.5, 30)
 plt.hist([Z_2, Z_10], randen, label=['$n=2$', '$n=10$'])
 plt.legend()
-plt.title('Histogram $Z_n$')
+plt.title('Simulatie normaalverdeling met $n$ aantal stochasten $X_k$ ~ $Un(0,1)$')
 plt.xlabel('$Z_n$')
 plt.ylabel('Frequentie')
 plt.show()
 
 # Opdracht 4
+stat2, p2 = stats.kstest(Z_2, 'norm')
+stat10, p10 = stats.kstest(Z_10, 'norm')
+print('KS-statistiek voor n=2 : ', stat2)
+print('KS-statistiek voor n=10 : ', stat10)
+
+# Opdracht 5
+n = [x for x in range(1, 26)]
+ks_stats = []
+
+for i in n:
+    statn, pn = stats.kstest(simuleer(i, 100000), 'norm')
+    ks_stats.append(statn)
+
+# Opdracht 6
+plt.scatter(n, ks_stats)
+plt.title('KS-statistieken tegen n aantal stochasten $X_k$ ~ $Un(0,1)$')
+plt.xlabel('$n$')
+plt.ylabel('KS-statistiek')
+plt.show()
+
+# Opdracht 7
