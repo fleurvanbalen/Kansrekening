ce6be501a2c33206711badce902b9a2ab9d554f3
Fleur van Balen <fleur.vanbalen@student.ru.nl>
2021-03-15 21:21:37 +0100

    W7

--- W7-FleurvanBalen.py
+++ W7-FleurvanBalen.py
@@ -31,8 +31,8 @@ plt.ylabel('Frequentie')
 plt.show()
 
 # Opdracht 4
-stat2, p2 = stats.kstest(Z_2, 'norm')
-stat10, p10 = stats.kstest(Z_10, 'norm')
+stat2 = stats.kstest(Z_2, 'norm')[0]
+stat10= stats.kstest(Z_10, 'norm')[0]
 print('KS-statistiek voor n=2 : ', stat2)
 print('KS-statistiek voor n=10 : ', stat10)
 
@@ -41,7 +41,7 @@ n = [x for x in range(1, 26)]
 ks_stats = []
 
 for i in n:
-    statn, pn = stats.kstest(simuleer(i, 100000), 'norm')
+    statn= stats.kstest(simuleer(i, 100000), 'norm')[0]
     ks_stats.append(statn)
 
 # Opdracht 6
@@ -52,3 +52,46 @@ plt.ylabel('KS-statistiek')
 plt.show()
 
 # Opdracht 7
+# voor een stochast X_k ~ Exp(1) geldt: verwachting mu = 1 en variantie sigma^2 = 1, dus sigma = 1
+mu_exp = 1
+sigma_2_exp = 1
+
+def inverse_exp(y):
+    return -(np.log(1-y))
+
+def simuleer_exp(n, m): # n is het aantal stochasten en m het aantal getallen
+    Z_n = []
+    for i in range(m):
+        S_n = 0
+        for j in range(n):
+            S_n += inverse_exp(rd.random()) # Optellen van n X_k stochasten met elk een verdeling volgens Exp(1)
+        Z_n.append((S_n-n*mu_exp)/(np.sqrt(sigma_2_exp*n))) # Berekenen van Z_n
+    return Z_n
+
+# Simuleer normaalverdeling
+Z_2_exp = simuleer_exp(2, 100000)
+Z_10_exp = simuleer_exp(10, 100000)
+
+# Tekenen van het histogram met Z_2_exp en Z_10_exp
+randen = np.linspace(-3.5, 3.5, 30)
+plt.hist([Z_2_exp, Z_10_exp], randen, label=['$n=2$', '$n=10$'])
+plt.legend()
+plt.title('Simulatie normaalverdeling met $n$ aantal stochasten $X_k$ ~ $Exp(1)$')
+plt.xlabel('$Z_n$')
+plt.ylabel('Frequentie')
+plt.show()
+
+n = [x for x in range(1, 26)]
+ks_stats_exp = []
+
+for i in n:
+    statn = stats.kstest(simuleer_exp(i, 100000), 'norm')[0]
+    ks_stats_exp.append(statn)
+
+# Scatterplot
+plt.scatter(n, ks_stats_exp)
+plt.title('KS-statistieken tegen n aantal stochasten $X_k$ ~ $Exp(1)$')
+plt.xlabel('$n$')
+plt.ylabel('KS-statistiek')
+plt.show()
+
